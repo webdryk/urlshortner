967bc30bb7466c9c1223d43ca773c85a94b463d4
webdryk <saamebley@gmail.com>
2025-04-14 15:40:30 +0000

    eslint error fixed

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/urlshortener";
-
+  process.env.MONGODB_URI 
 if (!MONGODB_URI) {
   throw new Error("Missing MongoDB URI");
 }
