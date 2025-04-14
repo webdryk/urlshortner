f2abc28308175ff130f413a6ba01f0007bf7411a
webdryk <saamebley@gmail.com>
2025-04-14 14:12:46 +0000

    eslint error fixed

---
+++ src/app/[code]/route.js
@@ -0,0 +1,27 @@
+// app/api/[code]/route.js
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/db";
+import Link from "@/lib/models/Link";
+
+export async function GET(request, { params }) {
+  try {
+    await dbConnect();
+    const { code } = params;
+
+    const link = await Link.findOne({ shortCode: code });
+    if (!link) {
+      return NextResponse.json({ error: "Link not found" }, { status: 404 });
+    }
+
+    link.clicks += 1;
+    link.clickHistory.push(new Date());
+    await link.save();
+
+    return NextResponse.redirect(link.originalUrl);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
